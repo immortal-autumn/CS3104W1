00a062711ef49021c58ec9cac81925ecd883bf79
hl74 <hl74@st-andrews.ac.uk>
2019-10-13 21:50:04 +0100

    13102019 successed

--- code/stacsbuild/myalloc.c
+++ code/stacsbuild/myalloc.c
@@ -29,9 +29,9 @@ Block is used to save meta data
 static const size_t block_size = sizeof(t_head); 
 static t_head * init = NULL;
 
-/* Create the initial memory zone when no memory allocated. */
+/* Create the critical initial memory zone when no memory allocated. */
 t_head * create_init() {
-	int size = 17 << 10; // 5 pages to malloc (.1.)
+	int size = 25 << 20; // 26214400 bytes, coverts to 25 MB in binary
 	t_head * new_zone = mmap(NULL, size, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS, 0, 0);
 	return new_zone;
 }
@@ -45,40 +45,46 @@ t_head * create_next(int size, void * addr) {
 	return block;
 }
 
+void * list_read(int size, t_head * node) {
+	if (node -> free == IS_FREE && node -> size >= size) {
+		node -> free = IS_USE;
+		return (void*)node + block_size;
+	}
+	if (node -> next == NULL) {
+		node -> next = create_next(size, (void*)node + block_size + node -> size);
+		return (void*)(node -> next) + block_size;
+	}
+	return list_read(size, node -> next);
+}
+
 /*
 malloc: read through the free list, find a bigger space; if not, create a new node
 We need a global initial block for storing the head node
 However we need to override the malloc first.
 */
 void * myalloc(int size){
+	printf("Processing: size %i\n", size);
 	if (size <= 0) return MALLOC_FAILED;
 	if (init == NULL) {
 		if (MAP_FAILED == (init = create_init())) return MALLOC_FAILED;
 		init -> size = size;
 		init -> next = NULL;
 		init -> free = IS_USE;
-		// printf("FREE BLOCK: %i, %lu\n", init -> free, &(init -> free));
 		return (void*)init + block_size;
 	}
 	else {
-		t_head * ptr = init;
-		while (ptr -> free == IS_USE || ptr -> size < size) {
-			if (ptr -> next == NULL) {
-				ptr -> next = create_next(size, ptr + block_size);
-				return ptr + block_size;
-			}
-		}
-		return ptr + block_size;
+		return list_read(size, init);
 	}
 }
 
 void myfree(void *ptr){
 	t_head * node = ptr - block_size;
-	// printf("FREE BLOCK: %i, %lu\n", node -> free, &(node -> free));
+	// printf("Node of %lu status: %i\n", ptr, node -> free);
 	if (node -> free == IS_USE) {
 		node -> free = IS_FREE;
 	}
 	else {
 		printf("Free failed!\n");
 	}
+	// printf("Node of %lu freed status: %i\n", ptr, node -> free);
 }
--- code/test.c
+++ code/test.c
@@ -8,16 +8,92 @@
 // work.
 // Kasim Terzic, Sep 2017
 
-int main(void)
-{
+//test1 simple malloc test
+void test1() {
+    //first malloc
     printf("Allocating 1024 * sizeof(int)... \n");
+    int* ptr1 = myalloc(1024*sizeof(int));
+    //second malloc
+    printf("Allocating 1024 * sizeof(int)... \n");
+    int* ptr2 = myalloc(521 * sizeof(int));
+    //third malloc
+    printf("Allocating 1024 * sizeof(int)... \n");
+    int* ptr3 = myalloc(1314 * sizeof(int));
 
-    int* ptr = myalloc(1024*sizeof(int));
-    ptr[1023] = 123;
-    // printf("%lu\n%lu\n", &ptr[0], ptr);
+    //first store
+    ptr1[0] = -1;
+    ptr1[1023] = -2;
+    printf("Array 1: address %lu, value: %i\n", &ptr1[0], ptr1[0]);
+    printf("Array 1: address %lu, value: %i\n", &ptr1[1023], ptr1[1023]);
+    ptr2[0] = -1;
+    ptr2[520] = -2;
+    printf("Array 2: address %lu, value: %i\n", &ptr2[0], ptr2[0]);
+    printf("Array 2: address %lu, value: %i\n", &ptr2[520], ptr2[520]);
+    ptr3[0] = -1;
+    ptr3[1313] = -2;
+    printf("Array 3: address %lu, value: %i\n", &ptr3[0], ptr3[0]);
+    printf("Array 3: address %lu, value: %i\n", &ptr3[1313], ptr3[1313]);
 
+
+    //first free
     printf("Freeing the allocated memory... \n");
-    myfree(ptr);
+    myfree(ptr1);
+    //second free
+    printf("Freeing the allocated memory... \n");
+    myfree(ptr2);
+    //third free
+    printf("Freeing the allocated memory... \n");
+    myfree(ptr3);
 
     printf("Yay!\n");
+}
+
+//free space use test
+void test2() {
+    //first malloc
+    printf("Allocating 1024 * sizeof(int)... \n");
+    int* ptr1 = myalloc(1024*sizeof(int));
+
+    //third malloc
+    printf("Allocating 1024 * sizeof(int)... \n");
+    int* ptr3 = myalloc(1314 * sizeof(int));
+
+    //first store
+    ptr1[0] = -1;
+    ptr1[1023] = -2;
+    printf("Array 1: address %lu, value: %i\n", &ptr1[0], ptr1[0]);
+    printf("Array 1: address %lu, value: %i\n", &ptr1[1023], ptr1[1023]);
+
+    //first free
+    printf("Freeing the allocated memory... \n");
+    myfree(ptr1);
+
+    //second malloc
+    printf("Allocating 1024 * sizeof(int)... \n");
+    int* ptr2 = myalloc(521 * sizeof(int));
+
+    ptr2[0] = -1;
+    ptr2[520] = -2;
+    printf("Array 2: address %lu, value: %i\n", &ptr2[0], ptr2[0]);
+    printf("Array 2: address %lu, value: %i\n", &ptr2[520], ptr2[520]);
+
+    ptr3[0] = -1;
+    ptr3[1313] = -2;
+    printf("Array 3: address %lu, value: %i\n", &ptr3[0], ptr3[0]);
+    printf("Array 3: address %lu, value: %i\n", &ptr3[1313], ptr3[1313]);
+
+
+    //second free
+    printf("Freeing the allocated memory... \n");
+    myfree(ptr2);
+    //third free
+    printf("Freeing the allocated memory... \n");
+    myfree(ptr3);
+
+    printf("Yay!\n");
+}
+
+int main(void)
+{
+    test2();
 }
